388b40efbffac2ce9735d1e3e97e1cac9a7490e0
shayanthyswamy <shayanthy.2004@gmail.com>
2025-05-18 14:55:43 +0530

    original

--- evenodd.c
+++ evenodd.c
@@ -4,3 +4,15 @@
 
 #include<conio.h>
 
+void main()
+{
+	int no;
+	Printf("enter the number");
+	scanf("%d", &no);
+	if (no % 2 == 0)
+		printf("Even");
+	else
+		printf("odd");
+}
+
+i have made changes 
